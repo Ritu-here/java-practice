f0f351202310440ef07108570391aea1fe5838a3
Ritu-here <ritu89159@gmail.com>
2025-09-18 14:56:16 +0530

    Add files via upload

---
+++ operatorsExample.java
@@ -0,0 +1,22 @@
+public class operatorsExample {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println("Arithmetic operator");
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a % b);
+
+
+
+        double x = 23.3;
+        double y = 12.3;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
