de8af185da151ccfb67cafd003d27f6be3806718
Ritu Kumari <ritu89159@gmail.com>
2025-09-23 22:40:52 +0530

    Add files via upload

---
+++ Parameter.java
@@ -0,0 +1,12 @@
+public class Parameter {
+    public static void main(String [] args){
+        int num = SumTwoNumber(4,5);
+        System.out.println(num);
+
+    }
+
+    public static int SumTwoNumber(int first , int second){
+        int sum = first + second;
+        return sum;
+    }
+}
