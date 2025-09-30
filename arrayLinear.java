57d6a7528a1805be1bd057fe52882f4665e6fe83
Ritu Kumari <ritu89159@gmail.com>
2025-09-30 20:28:20 +0530

    Add files via upload

---
+++ arrayLinear.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+public class arrayLinear {
+
+    public static int linearSearch(int arr[], int key){
+        for(int i = 0 ; i <= arr.length; i++){
+            if(arr[i] == key){
+                return i ;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,3,4,5,6,7 ,10};
+        String menu [] ={"dosa " , "samosa" , "chat" };
+        int key = 10;
+        int index = linearSearch( arr ,key );
+
+        if(index == -1){
+            System.out.println("index not found");
+        }
+        else{
+        System.out.println("Key is at index " + index );
+
+    }}
+}
