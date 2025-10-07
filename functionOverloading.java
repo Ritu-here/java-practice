1a00b6e9b232c1572d02b3c810382ee1c1c3595b
Ritu Kumari <ritu89159@gmail.com>
2025-10-07 23:14:39 +0530

    Add files via upload

---
+++ functionOverloading.java
@@ -0,0 +1,22 @@
+//function overloading
+
+public class functionOverloading {
+
+
+    public static int sum(int a , int b){
+        return a+b;
+    }
+
+    public static int sum(int a , int b, int c){
+        return a+b+c;
+    }
+    public static float sum(float a , float b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(2,6));
+        System.out.println( sum(2,5,3));
+        System.out.println(sum(2.2f ,2.1f));
+
+    }
+}
