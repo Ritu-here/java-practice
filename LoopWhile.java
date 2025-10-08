0035ced80ac9d3c36fdd99f8e91e19fd274b2615
Ritu Kumari <ritu89159@gmail.com>
2025-10-08 19:12:41 +0530

    Add files via upload

---
+++ LoopWhile.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+public class LoopWhile {
+    public static void main(String[] args) {
+        int num = 1;
+        while(num<= 10){
+            System.out.println(num);
+            num = num+1;
+
+        }
+        int count = 500;
+        while(count >= 200){
+            System.out.println(count);
+            count = count -1;
+        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number");
+        int i =0;
+        while(i < 5){
+            int inp = input.nextInt();
+            System.out.println("Number is :  " + inp);
+            i = i+1;
+        }
+
+
+    }
+}
